ebdb81fd6c038045e7dce5f1f5a9aaf3d0b533a6
Roman Stankiewicz <roman.stan3@gmail.com>
2018-07-08 10:13:44 +0100

    removed firebase conig

--- pages/firebase.js
+++
@@ -1,9 +0,0 @@
-
-const config = {
-  apiKey: "AIzaSyAbyiwARdEQbWgHf5Uwv0BBSOUfJtwfDBo",
-  authDomain: "specsavers-images.firebaseapp.com",
-  databaseURL: "https://specsavers-images.firebaseio.com",
-  projectId: "specsavers-images",
-};
-
-export default config
